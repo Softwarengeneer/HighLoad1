a654ee3a193cdc360a5ea1659b0e6350f6316d9a
nikita <nikitaspb2001@mail.ru>
2022-11-12 19:24:58 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/highload1/HighLoad1Application.kt
@@ -0,0 +1,11 @@
+package com.example.highload1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HighLoad1Application
+
+fun main(args: Array<String>) {
+	runApplication<HighLoad1Application>(*args)
+}
